e64a03756fd7f18bca9a483e61f59e6ad33468f4
Vadim <vadim.ganiev25@mail.ru>
2024-03-19 02:53:28 +0500

    Добавлен Maven, убраны косяки, теперь приложение может запускаться

--- src/main/java/org/robotoop/gui/LogWindow.java
+++ src/main/java/org/robotoop/gui/LogWindow.java
@@ -1,4 +1,4 @@
-package gui;
+package org.robotoop.gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -7,23 +7,21 @@ import java.awt.TextArea;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 
-import log.LogChangeListener;
-import log.LogEntry;
-import log.LogWindowSource;
+import org.robotoop.log.LogEntry;
+import org.robotoop.log.LogChangeListener;
+import org.robotoop.log.LogWindowSource;
 
-public class LogWindow extends JInternalFrame implements LogChangeListener
-{
+public class LogWindow extends JInternalFrame implements LogChangeListener {
     private LogWindowSource m_logSource;
     private TextArea m_logContent;
 
-    public LogWindow(LogWindowSource logSource) 
-    {
+    public LogWindow(LogWindowSource logSource) {
         super("Протокол работы", true, true, true, true);
         m_logSource = logSource;
         m_logSource.registerListener(this);
         m_logContent = new TextArea("");
         m_logContent.setSize(200, 500);
-        
+
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_logContent, BorderLayout.CENTER);
         getContentPane().add(panel);
@@ -31,20 +29,18 @@ public class LogWindow extends JInternalFrame implements LogChangeListener
         updateLogContent();
     }
 
-    private void updateLogContent()
-    {
+    private void updateLogContent() {
         StringBuilder content = new StringBuilder();
-        for (LogEntry entry : m_logSource.all())
-        {
+        for (LogEntry entry : m_logSource.all()) {
             content.append(entry.getMessage()).append("\n");
         }
         m_logContent.setText(content.toString());
         m_logContent.invalidate();
     }
-    
+
     @Override
-    public void onLogChanged()
-    {
+    public void onLogChanged() {
         EventQueue.invokeLater(this::updateLogContent);
     }
 }
+
---
+++ src/main/java/org/robotoop/log/LogChangeListener.java
@@ -0,0 +1,5 @@
+package org.robotoop.log;
+
+public interface LogChangeListener {
+    public void onLogChanged();
+}
